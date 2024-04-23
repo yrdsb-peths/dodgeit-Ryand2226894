094fdbcc5a08127fbf80462e297157647718cf96
Ryand2226894 <167019566+Ryand2226894@users.noreply.github.com>
2024-04-23 16:41:37 -0400

    move burger

--- burger.java
+++ burger.java
@@ -6,6 +6,19 @@ public class burger extends Actor
    
     public void act()
     {
-        move(-3);
+        move(-20);
+        
+        if(getX() <= 0) {
+            resetBurger();
+        }
     }
+    public void resetBurger() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        }else {
+            setLocation(600, 300);
+        }
+    }
+    
 }
